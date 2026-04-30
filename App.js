88a8c1834b8085f0ce84011d14ae1dfc815a38f3
harmalsingh770-eng <harmalsingh770@gmail.com>
2026-04-30 21:00:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Screens
 import LoginScreen from "./screens/LoginScreen";
 import Dashboard from "./screens/Dashboard";
 import ClassesScreen from "./screens/ClassesScreen";
@@ -9,30 +10,34 @@ import RequiredDocsScreen from "./screens/RequiredDocsScreen";
 import UploadDocScreen from "./screens/UploadDocScreen";
 import ReviewDocsScreen from "./screens/ReviewDocsScreen";
 import AdminDashboard from "./screens/AdminDashboard";
+import PaymentScreen from "./screens/PaymentScreen";
+import SchoolSetupScreen from "./screens/SchoolSetupScreen";
 
 export default function App() {
   return (
-    <div style={{ fontFamily: "Arial, sans-serif" }}>
-      <BrowserRouter>
-        <Routes>
-          {/* Auth */}
-          <Route path="/" element={<LoginScreen />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Auth */}
+        <Route path="/" element={<LoginScreen />} />
 
-          {/* Main */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/classes" element={<ClassesScreen />} />
-          <Route path="/students" element={<StudentsScreen />} />
-          <Route path="/add-student/:classId" element={<AddStudentScreen />} />
+        {/* Main */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/classes" element={<ClassesScreen />} />
+        <Route path="/students" element={<StudentsScreen />} />
+        <Route path="/add-student/:classId" element={<AddStudentScreen />} />
 
-          {/* Documents */}
-          <Route path="/required-docs" element={<RequiredDocsScreen />} />
-          <Route path="/upload-doc" element={<UploadDocScreen />} />
-          <Route path="/review-doc" element={<ReviewDocsScreen />} />
+        {/* Docs */}
+        <Route path="/required-docs" element={<RequiredDocsScreen />} />
+        <Route path="/upload-doc" element={<UploadDocScreen />} />
+        <Route path="/review-doc" element={<ReviewDocsScreen />} />
 
-          {/* Admin */}
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+        {/* Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* Optional */}
+        <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/setup-school" element={<SchoolSetupScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
