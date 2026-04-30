89321670f73537106460dcd50e988c3abffd7c54
harmalsingh770-eng <harmalsingh770@gmail.com>
2026-04-30 20:31:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-import React from "react";
-import AppNavigator from "./navigation/AppNavigator";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import Dashboard from "./screens/Dashboard";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif" }}>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
