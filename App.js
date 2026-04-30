a07a19c69ae8ab000da49efecd8bc3b33ea8be9c
harmalsingh770-eng <harmalsingh770@gmail.com>
2026-04-30 17:14:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <StatusBar style="light" />
-      <AppNavigator />
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
